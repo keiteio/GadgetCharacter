fa1d09dc5206090b8f32c7ee02259105a95c67e4
keiteio <mss.ymns@gmail.com>
2013-05-18 22:26:34 +0900

    メッセージ表示機能追加

--- scripts/common.js
+++ scripts/common.js
@@ -8,3 +8,20 @@ function current_character(){
   return character;
 }
 
+function readFile(filename){
+  var fso = new ActiveXObject("Scripting.FileSystemObject");
+  var str = "";
+  if(fso.FileExists(filename)){
+    var f = fso.OpenTextFile(filename, 1);
+    while (!f.AtEndOfStream){
+      str += f.ReadLine();
+    }
+    f.Close();
+  }
+  fso = null;
+  return str;
+}
+
+function getJsonFromFile(filename){
+  return $.parseJSON( readFile(filename) );
+}
--- scripts/graphics.js
+++ scripts/graphics.js
@@ -3,34 +3,24 @@ function loadCharacter(character_base_id){
   return getJsonFromFile(System.Gadget.path + "\\characters\\" + character_base_id + "\\character.json");
 }
 
-function readFile(filename){
-  var fso = new ActiveXObject("Scripting.FileSystemObject");
-  var str = "";
-  if(fso.FileExists(filename)){
-    var f = fso.OpenTextFile(filename, 1);
-    while (!f.AtEndOfStream){
-      str += f.ReadLine();
-    }
-    f.Close();
-  }
-  fso = null;
-  return str;
-}
-
-function getJsonFromFile(filename){
-  return $.parseJSON( readFile(filename) );
-}
-
 function changeCharacterImage(character){
   var img_path = "characters/" + character.base.id + "/images/"
   var img_src = img_path + character.base.images[ character.level ];
   $("#imgBackground").attr("src", img_src);
 }
 
-character.level = 2 // <= あとで消す
+function showMessage(){
+  System.Gadget.Flyout.file = "balloon.html";
+  System.Gadget.Flyout.show = true;
+  System.Gadget.Settings.write("character_base_id", current_character().base.id);
+  System.Gadget.Settings.write("character_level", current_character().level);
+}
+
+current_character().level = 0 // <= あとで消す
 
 $(document).ready(function(){
-  character.base = loadCharacter("type01");
-  $("#hw").text(character.base);//loadCharacter("type01").name);
+  current_character().base = loadCharacter("type01");
+  //$("#hw").text(character.base);//loadCharacter("type01").name);
   changeCharacterImage( current_character() );
+  showMessage();
 });
