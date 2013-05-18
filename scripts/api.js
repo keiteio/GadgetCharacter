21fae8855695134ed70087f43af1df3b55dc35f7
clayman <clayman964@gmail.com>
2013-05-18 23:17:35 +0900

    レベルアップカウント

--- scripts/api.js
+++ scripts/api.js
@@ -9,8 +9,16 @@ function GetItemFromDrop()
         // Display the current item property and increment the index.
         //spFeedback.innerHTML += oItem.size + "<br/>";
         current_character().totale_file_size = current_character().totale_file_size + oItem.size;
-        asdf = current_character().totale_file_size;
-        spFeedback.innerHTML = asdf;
+        spFeedback.innerHTML = current_character().totale_file_size
         intIndex++;
     }
+    var level =  current_character().level;
+    var level_margin =  current_character().base.level_up_value;
+    //spFeedback.innerHTML = current_character().base.level_up_value;
+    
+    if(level_margin[level + 1] < current_character().totale_file_size){
+    	current_character().level = level + 1;
+    	spFeedback.innerHTML = current_character().level;
+    }
+    
 }
